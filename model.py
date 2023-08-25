06c3e464893ac2272ada122e9b15b365b6cbafd9
I568348 <sachin.b.s@sap.com>
2023-08-25 11:27:10 +0530

    cast input

--- model.py
+++ model.py
@@ -191,6 +191,7 @@ class CustomSinePositionEncoding(tf.keras.layers.Layer):
         self.max_wavelength = max_wavelength
 
     def call(self, inputs: tf.Tensor):
+        inputs = tf.cast(inputs, tf.float32)
         shape = ops.shape(inputs)
         try:
             seq_length = shape[-2]
